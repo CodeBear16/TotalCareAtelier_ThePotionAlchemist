5de297b777753a3b46f048a2432bce4134fd01da
BigBear621 <ma626@naver.com>
2021-11-24 15:16:01 +0900

    맵(스타트맵, 게임맵) 구현중
    집가서 수정예정

---
+++ Assets/Alchemy/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController m_Instance;
+
+    public GameObject m_ActionText;
+
+    void Awake()
+    {
+        m_Instance = this;
+    }
+
+    internal void ShowActionText(bool _Show)
+    {
+       m_ActionText.SetActive(_Show);
+    }
+}
