4165367c152a407fd5d5a38b9845ff3b18ea282c
BigBear621 <rabbitbear621@gmail.com>
2021-11-25 14:55:42 +0900

    Essence Script

--- Assets/Scripts/Ingredient/IngredientContent/Seed.cs
+++ Assets/Scripts/Ingredient/IngredientContent/Seed.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Seed : MonoBehaviour
+public class Seed : Ingredient
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
