4432f2e543860ab9783fcb6476d71944c3999e29
peewoong <fridaymorning99@naver.com>
2021-12-08 09:41:23 +0900

    211208 video
    엔딩씬 비디오 추가

---
+++ Assets/Scripts/UI/VideoPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoPlayer : MonoBehaviour
+{
+    VideoPlayer video;
+    public int time;
+
+    private void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+        Invoke("GameEnd", time);
+    }
+
+    void GameEnd()
+    {
+        GameManager.instance.GameEndEvent();
+    }
+}
